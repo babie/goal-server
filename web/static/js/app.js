34afac9c19c3a6a96f2c4a2ad5d8fcfa64eee12c
babie <babie.wildheart@gmail.com>
2016-01-08 15:48:07 +0900

    Integrate react, flumpt, page.js npm packages

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,60 @@ import "../../../deps/phoenix_html/web/static/js/phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import React from 'react';  //eslint-disable-line
+import {Flux, Component} from 'flumpt';
+import ReactDOM from 'react-dom';
+import page from 'page';
+
+class CounterComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>count: {this.props.count}</p>
+        <div>
+          <button onClick={() => this.dispatch('increment')}>+1</button>
+          <button onClick={() => this.dispatch('decrement')}>-1</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class App extends Flux {
+  subscribe() {
+    this.on('increment', () => {
+      this.update(({count}) => {
+        return {count: count + 1};
+      });
+    });
+    this.on('decrement', () => {
+      this.update(({count}) => {
+        return {count: count - 1};
+      });
+    });
+  }
+  render(state) {
+    return <CounterComponent {...state}/>;
+  }
+}
+
+const app = new App({
+  renderer: el => {
+    ReactDOM.render(el, document.querySelector('.container'));
+  },
+  initialState: {count: 0},
+  middlewares: [
+    // logger
+    (state) => {
+      console.log(state);
+      return state;
+    }
+  ]
+});
+
+page('/goals/:id', function() {
+  app.update(_initialState => (_initialState));
+});
+page();
