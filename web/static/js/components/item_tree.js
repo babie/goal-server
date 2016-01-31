002948229499fe221f05fdb1b93af1514be80879
babie <babie.wildheart@gmail.com>
2016-01-31 20:39:12 +0900

    Change id,title,... props to node

--- web/static/js/components/item_tree.js
+++ web/static/js/components/item_tree.js
@@ -12,11 +12,11 @@ class ItemTreeComponent extends Component {
   }
 
   handleFocus(event) {
-    this.dispatch("goal:focus", this.props.id);
+    this.dispatch("goal:focus", this.props.node.model.id);
   }
 
   componentDidMount() {
-    if (this.props.id === this.props.self_and_ancestor_ids[0]) {
+    if (this.props.node.model.id === this.props.self_and_ancestor_ids[0]) {
       const [x, y] = this.calculatePosition();
       setTimeout(() => { 
         this.dispatch("goal:scroll", x, y);
@@ -26,7 +26,7 @@ class ItemTreeComponent extends Component {
   }
 
   componentDidUpdate() {
-    if (this.props.id === this.props.self_and_ancestor_ids[0]) {
+    if (this.props.node.model.id === this.props.self_and_ancestor_ids[0]) {
       const [x, y] = this.calculatePosition();
       this.dispatch("goal:scroll", x, y);
       this.refs.current.focus();
@@ -35,24 +35,24 @@ class ItemTreeComponent extends Component {
 
   render() {
     let descendants_tree = null;
-    if (this.props.descendants) {
-      descendants_tree = this.props.descendants.map((c, i) => {
-        return <ItemTreeComponent key={c.id} {...c} self_and_ancestor_ids={this.props.self_and_ancestor_ids} h={this.props.h + 1} v={this.props.v + i} />;
+    if (!_.isEmpty(this.props.node.children)) {
+      descendants_tree = this.props.node.children.map((n, i) => {
+        return <ItemTreeComponent key={n.model.id} node={n} self_and_ancestor_ids={this.props.self_and_ancestor_ids} h={this.props.h + 1} v={this.props.v + i} />;
       });
     }
     let openClass = null;
-    if (_.some(this.props.self_and_ancestor_ids, (v) => (v === this.props.id || v === this.props.parent_id))) {
+    if (_.some(this.props.self_and_ancestor_ids, (v) => (v === this.props.node.model.id || v === this.props.node.model.parent_id))) {
       openClass = "open";
     }
     let currentClass = null;
-    if (this.props.id === this.props.self_and_ancestor_ids[0]) {
+    if (this.props.node.model.id === this.props.self_and_ancestor_ids[0]) {
       currentClass = "current";
     }
 
     return (
       <li className={openClass}>
         <section className={currentClass} tabIndex={0} onFocus={this.handleFocus.bind(this)} onClick={this.handleFocus.bind(this)} ref="current">
-          {this.props.title}
+          {this.props.node.model.title}
         </section>
         <ul>
           {descendants_tree}
