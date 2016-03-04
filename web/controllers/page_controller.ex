805d4cfa549f774a69ca593d9e8ce8767bc4bc8f
babie <babie.wildheart@gmail.com>
2016-03-04 22:37:02 +0900

    Remove css check page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,8 +4,4 @@ defmodule GoalServer.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
-
-  def columned_treeview(conn, _params) do
-    render conn, "columned_treeview.html"
-  end
 end
